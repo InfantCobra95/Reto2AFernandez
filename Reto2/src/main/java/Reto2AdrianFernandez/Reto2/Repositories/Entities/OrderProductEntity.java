34a28b182db0cbcce0b1114563c3a90ad814644c
InfantCobra95 <adrianfernandezabuelo@gmail.com>
2021-03-07 18:07:51 +0100

    Entities Terminadas

--- Reto2/src/main/java/Reto2AdrianFernandez/Reto2/Repositories/Entities/OrderProductEntity.java
+++ Reto2/src/main/java/Reto2AdrianFernandez/Reto2/Repositories/Entities/OrderProductEntity.java
@@ -1,5 +1,72 @@
 package Reto2AdrianFernandez.Reto2.Repositories.Entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Table(name = "orderproducts")
+@Entity(name = "OrderProducts")
 public class OrderProductEntity {
-    
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "order_id")
+  private OrderEntity orderEntity;
+
+  
+  @ManyToOne
+  @JoinColumn(name = "product_id")
+  private ProductEntity productEntity;
+
+  private int quantity;
+
+  public OrderProductEntity(
+    ProductEntity productEntity,
+    OrderEntity orderEntity,
+    int quantity
+  ) {
+    this.orderEntity = orderEntity;
+    this.productEntity = productEntity;
+    this.quantity = quantity;
+  }
+
+  public OrderProductEntity() {}
+
+  public void setQuantity(int quantity) {
+    this.quantity = quantity;
+  }
+
+  public int getQuantity() {
+    return quantity;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public OrderEntity getOrderEntity() {
+    return orderEntity;
+  }
+
+  public ProductEntity getProductEntity() {
+    return productEntity;
+  }
+
+  public void setOrderEntity(OrderEntity orderEntity) {
+    this.orderEntity = orderEntity;
+  }
+
+  public void setProductEntity(ProductEntity productEntity) {
+    this.productEntity = productEntity;
+  }
 }
--- Reto2/src/main/java/Reto2AdrianFernandez/Reto2/Repositories/Entities/ProductEntity.java
+++ Reto2/src/main/java/Reto2AdrianFernandez/Reto2/Repositories/Entities/ProductEntity.java
@@ -1,5 +1,65 @@
 package Reto2AdrianFernandez.Reto2.Repositories.Entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "products")
+@Entity(name = "products")
 public class ProductEntity {
-    
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String nombre;
+  private double precio;
+  private String img;
+
+  public ProductEntity() {}
+
+  public ProductEntity(String nombre, double precio, String img) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.img = img;
+  }
+
+  public ProductEntity(ProductEntity p) {
+    this.nombre = p.getNombre();
+    this.precio = p.getPrecio();
+    this.img = p.getImg();
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
+
+  public double getPrecio() {
+    return precio;
+  }
+
+  public void setPrecio(double precio) {
+    this.precio = precio;
+  }
+
+  public String getImg() {
+    return img;
+  }
+
+  public void setImg(String img) {
+    this.img = img;
+  }
 }
