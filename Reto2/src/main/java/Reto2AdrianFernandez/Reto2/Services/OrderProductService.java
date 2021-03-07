5eb99cc8c75528bcced0eebfcd004129a8d12070
InfantCobra95 <adrianfernandezabuelo@gmail.com>
2021-03-08 00:38:52 +0100

    Insertar Peridos con los productos en OrderProducts

--- Reto2/src/main/java/Reto2AdrianFernandez/Reto2/Services/OrderProductService.java
+++ Reto2/src/main/java/Reto2AdrianFernandez/Reto2/Services/OrderProductService.java
@@ -7,7 +7,9 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import Reto2AdrianFernandez.Reto2.Repositories.Entities.OrderEntity;
 import Reto2AdrianFernandez.Reto2.Repositories.Entities.OrderProductEntity;
+import Reto2AdrianFernandez.Reto2.Repositories.Entities.ProductEntity;
 import Reto2AdrianFernandez.Reto2.Repositories.Interfaces.OrderProductRepository;
 import Reto2AdrianFernandez.Reto2.Repositories.Interfaces.OrderRepository;
 import Reto2AdrianFernandez.Reto2.Repositories.Interfaces.ProductRepository;
@@ -49,5 +51,30 @@ public class OrderProductService {
     }
   }
   */
+
+  public Long InsertOrderProducts(List<OrderProductDTO> newOrderProducts) {
+    Long oderId = null;
+    boolean firstTime = false;
+    for (OrderProductDTO newOrderProduct : newOrderProducts) {
+      ProductEntity p = null;
+      OrderEntity o = null;
+      if (newOrderProduct.getProductEntity() != null) {
+        p = new ProductEntity(newOrderProduct.getProductEntity());
+        productRepository.save(p);
+      }
+
+      if (newOrderProduct.getOrderEntity() != null) {
+        o = new OrderEntity(newOrderProduct.getOrderEntity());
+        orderRepository.save(o);
+      }
+
+      OrderProductEntity op = new OrderProductEntity(p, o, newOrderProduct.getQuantity());
+      if (!firstTime) {
+        oderId = op.getOrderEntity().getId();
+      }
+      orderProductRepository.save(op);
+    }
+    return oderId;
+  }
 
 }
